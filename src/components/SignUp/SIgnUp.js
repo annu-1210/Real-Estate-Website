3eb86e39abf9e2eda1f331cfedc4bcefbb5553da
Annu Poonia <annupoonia086@gmail.com>
2024-05-19 19:17:25 +0530

    changes made

--- src/components/SignUp/SIgnUp.js
+++ src/components/SignUp/SIgnUp.js
@@ -18,11 +18,52 @@ function SIgnUp() {
   });
   const navigate = useNavigate();
 
+  const validation = () => {
+    let isProceed = true;
+    let errorMessage = 'Please Enter Valid '
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!input.username) {
+      isProceed = false;
+      errorMessage += ' Username'
+    }
+    if (!input.name) {
+      isProceed = false;
+      errorMessage += ' name'
+    }
+    if (!input.email) {
+      isProceed = false;
+      errorMessage += ' email'
+    }
+    if (!input.password) {
+      isProceed = false;
+      errorMessage += ' password'
+    } else if (input.password.length < 6) {
+      toast.error("Password must be more than 6 characters");
+    }else if(input.password.length > 10){
+      toast.error("Password must be less than 10 characters");
+    }
+
+    if(!isProceed){
+      toast.error(errorMessage);
+    } else {
+      if (!regex.test(input.email)) {
+        toast.error("Please Enter Valid email type");
+    }
+  }
+  return isProceed;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem("user", JSON.stringify(input));
-    toast.success("Registered Successfully!");
-    navigate("/login");
+    if (validation()) {
+      localStorage.setItem("user", JSON.stringify(input));
+
+      toast.success("Registered Successfully!");
+      navigate("/login");
+    }
+    else {
+      toast.error("Please Enter Valid Credentials");
+    }
   };
   useEffect(() => {
     AOS.init({ duration: 2000 });
@@ -49,9 +90,9 @@ function SIgnUp() {
             <input
               name="name"
               value={input.name}
-              onChange={(e) =>
-                setInput({ ...input, [e.target.name]: e.target.value })
-              }
+              onChange={(e) => {
+                setInput({ ...input, [e.target.name]: e.target.value });
+              }}
               type="text"
               placeholder="enter name"
             />
